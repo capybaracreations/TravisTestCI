b97283f0fda6b827e81634bef8bb36f42b5c2e82
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2016-04-29 00:52:30 +0200

    no message

--- app/src/main/java/com/blstream/studybox/MainActivity.java
+++ app/src/main/java/com/blstream/studybox/MainActivity.java
@@ -1,13 +1,34 @@
 package com.blstream.studybox;
 
+import android.content.pm.ApplicationInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isDebuggable;
+private TextView textview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = (TextView) findViewById(R.id.textview);
+
+        ApplicationInfo ai = getApplication().getApplicationInfo();
+
+        if(ai != null) {
+            isDebuggable = (ai.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        }
+
+        if (isDebuggable) {
+            textview.setText("DEBUG");
+        } else {
+            textview.setText("RELEASE");
+        }
+
     }
 }
